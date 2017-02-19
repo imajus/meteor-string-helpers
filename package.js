39461b4b2976704337db1adead71d6837f8518df
Denis Perov <denis.perov@gmail.com>
2017-02-19 11:55:55 +0300

    update package info

--- package.js
+++ package.js
@@ -1,25 +1,25 @@
 Package.describe({
-  name: 'majus:string-helpers',
+  name: 'imajus:string-helpers',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'JavaScript String related Spacebars helpers.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/imajus/meteor-string-helpers.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: null//'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4.2');
-  api.use('ecmascript');
-  api.use(['templating', 'underscore', 'majus:helpers-core@0.0.1'], 'client');
+  api.use('ecmascript@0.6.3');
+  api.use(['templating@1.3.0', 'underscore@1.0.10', 'imajus:helpers-core@0.0.1'], 'client');
   api.mainModule('string-helpers.js', 'client');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('majus:string-helpers', 'client');
+  api.use('ecmascript@0.6.3');
+  api.use('tinytest@1.0.12');
+  api.use('imajus:string-helpers', 'client');
   api.mainModule('string-helpers-tests.js', 'client');
 });
