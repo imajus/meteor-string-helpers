ca6decacd9058175e12da0befb8f88e128e70fc7
Denis Perov <denis.perov@gmail.com>
2018-07-02 00:00:57 +0300

    Increment package version

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'imajus:string-helpers',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
-  summary: 'JavaScript String related Spacebars helpers.',
+  summary: 'Blaze helpers for string manipulation.',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/imajus/meteor-string-helpers.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: null//'README.md'
+  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
